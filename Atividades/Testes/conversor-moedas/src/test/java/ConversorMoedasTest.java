4e29ddde23039a8c4ce90bf2a6f13f78d08c0e1d
Rodrigo <roosoares.desenvolvedor@gmail.com>
2024-10-22 10:59:03 -0300

    Add Primeira Etapa TDD

---
+++ Atividades/Testes/conversor-moedas/src/test/java/ConversorMoedasTest.java
@@ -0,0 +1,22 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ConversorMoedasTest {
+
+    @Test
+    public void deveConverterUSDParaBRL() {
+        
+        TaxaCambioProvider taxaMock = mock(TaxaCambioProvider.class);
+        when(taxaMock.obterTaxaCambio("USD", "BRL")).thenReturn(5.0);
+
+        ConversorMoedas conversor = new ConversorMoedas(taxaMock);
+
+        double resultado = conversor.converter("USD", "BRL", 10);
+
+        assertEquals(50.0, resultado);
+    }
+}
